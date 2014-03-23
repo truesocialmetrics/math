1862fe7a757c2224ea127b9a9bb12b0d81fb93b7
Rostislav Mykhajliw <necromant2005@gmail.com>
2014-03-23 07:51:45 +0200

    running average not fit window

--- tests/TweeMath/Algorithm/RunningAverageTest.php
+++ tests/TweeMath/Algorithm/RunningAverageTest.php
@@ -10,4 +10,11 @@ class RunningAverageTest extends PHPUnit_Framework_TestCase
         $output = array(1 => 1,  4 => 4, 6 => 2);
         $this->assertEquals($output, RunningAverage::apply($input, 2));
     }
+
+    public function testNotFitWindow()
+    {
+        $input = array(0 => 0, 1 => 2, 3 => 3, 4 => 5, 5 => 3);
+        $output = array(1 => 1,  4 => 4);
+        $this->assertEquals($output, RunningAverage::apply($input, 2));
+    }
 }
